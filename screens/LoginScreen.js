6ecab78ebaea03b222713bb229b58a344ceb7d09
SolomiiaKaminska <solomija0405@gmail.com>
2021-04-24 18:52:42 +0300

    added Login SignUp and menu components

---
+++ screens/LoginScreen.js
@@ -0,0 +1,199 @@
+import React, { useState, useContext } from "react";
+import {
+  StyleSheet,
+  Text,
+  TextInput,
+  View,
+  Image,
+  TouchableOpacity,
+} from "react-native";
+import { AuthContext } from "../navigation/AuthProvider";
+
+export default function LoginScreen({ navigation }) {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const { login } = useContext(AuthContext);
+  return (
+  <View style={styles.container}>
+        <View style={styles.mainView}>
+          <Image
+            style={styles.mainImage}
+            source={require("../images/mainImg.jpg")}
+          />
+          <Text
+            style={{
+              color: "#C5C4C4",
+              fontSize: 30,
+              marginTop: 10,
+              fontWeight: "700",
+            }}
+          >
+            Cake app
+          </Text>
+          <Text
+            style={{
+              color: "#C7C7C7",
+              fontSize: 15,
+              textAlign: "center",
+              marginTop: 10,
+              fontWeight: "400",
+            }}
+          >
+            Please log in to your account to continue work in app
+          </Text>
+          <Text style={styles.label}>Email</Text>
+          <TextInput
+            style={styles.input}
+            onChangeText={(userEmail) => setEmail(userEmail)}
+            value={email}
+            placeholder="cakeuser@gmail.com"
+            keyboardType="default"
+            textAlign="center"
+          />
+          <Text style={styles.label}>Password</Text>
+          <TextInput
+            style={styles.input}
+            onChangeText={(userPassword) => setPassword(userPassword)}
+            value={password}
+            secureTextEntry={true}
+            placeholder="your password"
+            keyboardType="numeric"
+            dataDetectorTypes="phoneNumber"
+            textAlign="center"
+          />
+          <TouchableOpacity
+            style={{ alignItems: "stretch" }}
+            onPress={() => navigation.navigate("Signup")}
+          >
+            <Text style={{ color: "#C5C4C4", fontSize: 13, marginTop: 20 }}>
+              Create account,<Text> </Text>
+              <Text
+                style={{
+                  color: "#C5C4C4",
+                  fontSize: 16,
+                  textDecorationLine: "underline",
+                }}
+              >
+                Sign up
+              </Text>
+            </Text>
+          </TouchableOpacity>
+
+          <TouchableOpacity
+            style={{ marginTop: 20 }}
+            onPress={() => {
+            try{
+            let a = login(email, password);
+            console.log(a);
+
+            }catch (e){
+            console.log(e)
+            }
+            }}
+          >
+            <View style={styles.logbtn}>
+              <Text
+                style={{
+                  color: "#fff",
+                  fontSize: 17,
+                  fontWeight: "700",
+                  padding: 17,
+                }}
+              >
+                Log in
+              </Text>
+            </View>
+          </TouchableOpacity>
+        </View>
+      </View>
+    );
+  };
+  const styles = StyleSheet.create({
+    container: {
+      backgroundColor: "#fff",
+      alignItems: "center",
+      justifyContent: "center",
+    },
+    mainView: {
+      justifyContent: "center",
+      alignItems: "center",
+      height: "100%",
+      width: "80%",
+    },
+    mainImage: {
+      height: 95,
+      width: 95,
+      borderRadius: 20,
+      borderWidth: 2,
+      borderColor: "#000",
+      marginTop: 0,
+    },
+    input: {
+      borderRadius: 15,
+      borderWidth: 1,
+      height: 50,
+      width: 310,
+      fontSize: 16,
+    },
+    label: {
+      color: "#C5C4C4",
+      fontSize: 14,
+      textAlign: "center",
+      marginTop: 25,
+      marginBottom: 9,
+      fontWeight: "700",
+    },
+    logbtn: {
+      backgroundColor: "#000",
+      width: 310,
+      borderRadius: 100,
+      justifyContent: "center",
+      alignItems: "center",
+    },
+  });
+//    <View style={styles.container}>
+//      <Text style={styles.text}>Welcome to Firebase app</Text>
+//      <FormInput
+//        value={email}
+//        placeholderText="Email"
+//        onChangeText={(userEmail) => setEmail(userEmail)}
+//        autoCapitalize="none"
+//        keyboardType="email-address"
+//        autoCorrect={false}
+//      />
+//      <FormInput
+//        value={password}
+//        placeholderText="Password"
+//        onChangeText={(userPassword) => setPassword(userPassword)}
+//        secureTextEntry={true}
+//      />
+//      <FormButton buttonTitle="Login" onPress={() => login(email, password)} />
+//      <TouchableOpacity
+//        style={styles.navButton}
+//        onPress={() => navigation.navigate("Signup")}
+//      >
+//        <Text style={styles.navButtonText}>New user? Join here</Text>
+//      </TouchableOpacity>
+//    </View>
+//  );
+//}
+//
+//const styles = StyleSheet.create({
+//  container: {
+//    backgroundColor: "#f5f5f5",
+//    flex: 1,
+//    justifyContent: "center",
+//    alignItems: "center",
+//  },
+//  text: {
+//    fontSize: 24,
+//    marginBottom: 10,
+//  },
+//  navButton: {
+//    marginTop: 15,
+//  },
+//  navButtonText: {
+//    fontSize: 20,
+//    color: "#6646ee",
+//  },
+//});
---
+++ screens/SignUpScreen.js
@@ -0,0 +1,156 @@
+import React, { useState,useEffect, useContext } from "react";
+import {
+  StyleSheet,
+  Text,
+  TextInput,
+  View,
+  Image,
+  TouchableOpacity,
+} from "react-native";
+import { AuthContext } from "../navigation/AuthProvider";
+
+export default function SignupScreen() {
+  const [email, setEmail] = useState("");
+  const [passwordCheck, setPasswordCheck] = useState("");
+  const [password, setPassword] = useState("");
+  const [fullName, setFullName] = useState("");
+  const { register } = useContext(AuthContext);
+  const [isDisable,setIsDisable] = useState(true);
+  function validateEmail(email) {
+      const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+      console.log(re.test(String(email).toLowerCase()))
+      return re.test(String(email).toLowerCase());
+  }
+
+  useEffect(()=>{
+  if (email && password && passwordCheck){
+  setIsDisable(false)
+  }
+  },[email,password,passwordCheck])
+  return (
+  <View style={styles.container}>
+        <View style={styles.mainView}>
+            <View
+            style={{
+              width: "100%",
+              textAlign: "left"
+            }}
+          >
+            <Text style={styles.label}>E-mail</Text>
+          </View>
+          <TextInput
+            style={styles.input}
+            onChangeText={(userEmail) => setEmail(userEmail)}
+            value={email}
+            placeholder="cakeuser@gmail.com"
+            keyboardType="default"
+            textAlign="center"
+          />
+          <View
+            style={{
+              width: "100%",
+              textAlign: "left",
+            }}
+          >
+            <Text style={styles.label}>Password</Text>
+          </View>
+          <TextInput
+            style={styles.input}
+            value={passwordCheck}
+            onChangeText={(userPassword) => setPasswordCheck(userPassword)}
+            secureTextEntry={true}
+            placeholder="Enter your password"
+            keyboardType="default"
+            textAlign="center"
+          />
+          <Text style={{ color: "#C5C4C4", fontSize: 13, marginTop: 5 }}>
+            Your password must be 8-16 characters and contain at least one number
+            dagit
+          </Text>
+          <View
+            style={{
+              width: "100%",
+              textAlign: "left",
+            }}
+          >
+            <Text style={styles.label}>Confirm Password</Text>
+          </View>
+          <TextInput
+            style={styles.input}
+            value={password}
+            onChangeText={(userPassword) => setPassword(userPassword)}
+            secureTextEntry={true}
+            placeholder="Enter your password again"
+            keyboardType="default"
+            textAlign="center"
+          />
+
+          <TouchableOpacity
+            disabled= {isDisable}
+            style={{ marginTop: 40 }}
+            onPress={() =>{
+            if (validateEmail(email) && password.length >=8 && password === passwordCheck)
+            {return register(email, password)}
+            else alert("Failed to register") }}
+          >
+            <View style={styles.logbtn}>
+              <Text
+                style={{
+                  color: "#fff",
+                  fontSize: 17,
+                  fontWeight: "700",
+                  padding: 17,
+                }}
+              >
+                Sign up
+              </Text>
+            </View>
+          </TouchableOpacity>
+        </View>
+      </View>
+    );
+  };
+
+  const styles = StyleSheet.create({
+    container: {
+    height:"100%",
+      backgroundColor: "#fff",
+      alignItems: "center",
+    },
+    mainView: {
+      alignItems: "center",
+      height: "90%",
+      width: "80%",
+      justifyContent:"center",
+
+    },
+    input: {
+      borderRadius: 10,
+      borderWidth: 1,
+      height: 46,
+      width: 310,
+      fontSize: 16,
+    },
+    label: {
+      color: "#C5C4C4",
+      fontSize: 14,
+      marginTop: 10,
+      marginBottom: 9,
+      fontWeight: "700",
+      textAlign: "left",
+    },
+    logbtn: {
+      backgroundColor: "#000",
+      width: 310,
+      borderRadius: 100,
+      justifyContent: "center",
+      alignItems: "center",
+    },
+    inputLast: {
+      borderRadius: 10,
+      borderWidth: 1,
+      height: 46,
+      width: 310,
+      fontSize: 16,
+    },
+  });
